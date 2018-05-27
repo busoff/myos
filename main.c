049984f7e134172dcf0b514ff639fcff027b3eb0
busoff <busoff@qq.com>
2018-05-28 00:33:49 +0800

    trigger divided-by-0 exception

--- main.c
+++ main.c
@@ -5,50 +5,6 @@
 #include "arch/idt.h"
 #include "driver/screen.h"
 
-// /**
-//  * @brief print a char at a given position on a screen 80 * 25
-//  */
-// void printAt(const char* str, uint8_t x, uint8_t y)
-// {
-//     if (str == 0)
-//     {
-//         return;
-//     }
-
-//     if ( x >= 80 || y >= 25)
-//     {
-//         return;
-//     }
-
-//     /* a chararcter shown on screen is represented as uint16_t
-//      * lowest 8 bit is ASCII and high 8 bits are color
-//      */
-//     volatile uint16_t * video_buffer = (volatile uint16_t*) 0xB8000;
-//     const uint8_t white_color = 15;
-
-//     uint8_t x_pos = x;
-//     uint8_t y_pos = y;
-
-//     const char* s = str;
-//     while(*s != 0)
-//     {
-//         video_buffer[x_pos + 80 * y_pos] = *s | white_color << 8;
-
-//         ++x_pos;
-//         if (x_pos >= 80)
-//         {
-//             x_pos = 0;
-//             y_pos++;
-//             if ( y_pos >= 25 )
-//             {
-//                 y_pos = 0;
-//             }
-//         }
-
-//         s++;
-//     }
-// }
-
 void kmain(void* multiboot_information, uint32_t magic_number)
 {
     (void) multiboot_information;
@@ -59,6 +15,10 @@ void kmain(void* multiboot_information, uint32_t magic_number)
 
     scr_print("Hello MyOS\nhello again");
 
+    int b = 0;
+    int a = 1/b;
+    (void)a;
+    // trigger divide-by-0 exception
     while(1)
     {
 
