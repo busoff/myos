75cf8bd2fbce2149d06c4899f4d4742c3b088a95
busoff <busoff@qq.com>
2018-12-27 00:46:10 +0800

    use irq number

--- driver/keyboard.c
+++ driver/keyboard.c
@@ -1,15 +1,18 @@
 #include "driver/keyboard.h"
-#include "arch/isr.h"
+#include "arch/irq.h"
 #include "kstdlib.h"
+#include "arch/port.h"
 
-static const uint32_t KEYBOARD_IRQ = 32;
-
+static const uint32_t KEYBOARD_IRQ = 1;
+static const uint8_t DATA_PORT = 0x60;
 static void keyboard_isr(struct regs regs)
 {
-    kprintf("keyboard hit %d\n", regs.interrupt_num);
+    (void)regs;
+    uint8_t scancode = inb(DATA_PORT);
+    kprintf("0x%x \n", scancode);
 }
 
 void keyboard_init()
 {
-    isr_register(KEYBOARD_IRQ, &keyboard_isr);
+    irq_register(KEYBOARD_IRQ, &keyboard_isr);
 }
